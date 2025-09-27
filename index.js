80d9610b47db1907000d3a4c7c7f58f82a63585c
JakeConal <23120124@student.hcmus.edu.vn>
2025-09-27 15:36:15 +0700

    fix: Hello world

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World from Render!\n');
+  res.end('Hello World!\n');
 });
 
 server.listen(PORT, () => {
