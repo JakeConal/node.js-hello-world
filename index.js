1ea85186809dbb77f528ef05caad198c44949661
JakeConal <23120124@student.hcmus.edu.vn>
2025-09-27 15:19:04 +0700

    feat: add initial Node.js Hello World app

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const http = require('http');
 
+const PORT = process.env.PORT || 3000;
+
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
+  res.end('Hello World from Render!\n');
 });
 
-server.listen(3000, () => {
-  console.log('Server running at http://localhost:3000/');
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
